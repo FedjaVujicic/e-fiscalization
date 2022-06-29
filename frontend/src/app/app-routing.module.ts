8a62039332d2e5287b3872a77046ddb981df20e4
Fedja Vujicic <fedjavujicic@gmail.com>
2022-06-29 14:51:12 +0200

    Page template design with header and footer

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginAdminComponent } from './login-admin/login-admin.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "/login",
+    pathMatch: "full",
+  },
+  {
+    path: "login",
+    component: LoginComponent,
+  },
+  {
+    path: "register",
+    component: RegisterComponent,
+  },
+  {
+    path: "admin",
+    component: LoginAdminComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
