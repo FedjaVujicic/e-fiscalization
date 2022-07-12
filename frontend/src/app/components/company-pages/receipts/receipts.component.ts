b24973654d02924e79e8cf05e4fc0b9df41b6138
Fedja Vujicic <fedjavujicic@gmail.com>
2022-07-13 01:24:48 +0200

    Started working on the receipts page

--- frontend/src/app/components/company-pages/receipts/receipts.component.ts
+++ frontend/src/app/components/company-pages/receipts/receipts.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Company } from 'src/app/models/company';
+import { Product } from 'src/app/models/product';
+import { ProductService } from "src/app/services/product/product.service"
 
 @Component({
   selector: 'app-receipts',
@@ -7,9 +10,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReceiptsComponent implements OnInit {
 
-  constructor() { }
+  company: Company;
+  selectedType: string;
+  selectedName: string;
+  selectedWarehouse: string;
+  selectedCashRegister: string;
+  allProducts: Array<Product> = [];
+  confirmedFacility = false;
+  filteredProducts: Array<Product> = [];
+
+  message: string;
+
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
+    this.company = JSON.parse(localStorage.getItem("logged"));
+    this.productService.getAllProducts(this.company.username).subscribe((allProducts: Array<Product>) => {
+      this.allProducts = allProducts;
+    });
+  }
+
+  confirmFacility(): void {
+    if ((this.selectedType === "shop" && this.selectedCashRegister) ||
+      (this.selectedType === "warehouse" && this.selectedWarehouse)) {
+      this.filteredProducts =
+        this.allProducts.filter(product => {
+          for (let i = 0; i < product.facilities.length; ++i) {
+            if (this.selectedType === "shop" && product.facilities[i].name === this.selectedCashRegister) {
+              return true;
+            }
+            else if (this.selectedType === "warehouse" && product.facilities[i].name === this.selectedWarehouse) {
+              return true;
+            }
+          }
+          return false;
+        });
+      this.confirmedFacility = true;
+    }
+    else {
+      this.message = "Izaberite lokal";
+    }
+  }
+
+  backToSelect(): void {
+    this.confirmedFacility = false;
   }
 
 }
