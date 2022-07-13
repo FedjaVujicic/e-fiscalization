6918aff16b89c8b7c0517346663862ffbb7f3570
Fedja Vujicic <fedjavujicic@gmail.com>
2022-07-13 15:05:22 +0200

    Receipt creation

--- frontend/src/app/components/company-pages/receipts-store/receipts-store.component.ts
+++ frontend/src/app/components/company-pages/receipts-store/receipts-store.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Company } from 'src/app/models/company';
 import { Facility } from 'src/app/models/facility';
 import { Product } from 'src/app/models/product';
+import { Receipt } from 'src/app/models/receipt';
+import { Item } from 'src/app/models/item';
 import { ProductService } from 'src/app/services/product/product.service';
 
 @Component({
@@ -16,6 +18,7 @@ export class ReceiptsStoreComponent implements OnInit {
   confirmedFacility = false;
   filteredProducts: Array<Product> = [];
   currentFacility: Facility = new Facility();
+  currentReceipt: Receipt = new Receipt();
 
   message: string;
 
@@ -27,6 +30,12 @@ export class ReceiptsStoreComponent implements OnInit {
     this.productService.getAllProducts(this.company.username).subscribe((allProducts: Array<Product>) => {
       this.allProducts = allProducts;
     });
+
+    //Receipt initialization
+    this.currentReceipt.companyName = this.company.name;
+    this.currentReceipt.totalPriceNoTax = 0;
+    this.currentReceipt.totalPriceTax = 0;
+    this.currentReceipt.date = new Date();
   }
 
   confirmFacility(): void {
@@ -41,6 +50,7 @@ export class ReceiptsStoreComponent implements OnInit {
           return false;
         });
       this.confirmedFacility = true;
+      this.currentReceipt.facility = this.currentFacility;
     }
     else {
       this.message = "Izaberite lokal";
@@ -49,14 +59,50 @@ export class ReceiptsStoreComponent implements OnInit {
 
   backToSelect(): void {
     this.confirmedFacility = false;
+    delete this.currentReceipt;
+    this.currentReceipt = new Receipt();
   }
 
   // Gets the index of the current facility in the given products facilities list  
-  getIndex(product: Product): number {
+  getFacilityIndex(product: Product): number {
     if (product.facilities) {
       return product.facilities.map(facility => facility.name).indexOf(this.currentFacility.name);
     }
-    return 0;
+    return -1;
+  }
+
+  getItemIndex(name: string): number {
+    if (this.currentReceipt.items) {
+      return this.currentReceipt.items.map(item => item.name).indexOf(name);
+    }
+    return -1;
+  }
+
+
+
+  addItem(product: Product) {
+    // Initialize the new item
+    let currentItem: Item = new Item();
+    currentItem.name = product.name;
+    currentItem.unit = product.unit;
+    currentItem.tax = product.tax;
+    currentItem.priceNoTax = product.facilities[this.getFacilityIndex(product)].priceSell;
+    this.currentReceipt.totalPriceNoTax += currentItem.priceNoTax + 0;
+    currentItem.priceTax = currentItem.priceNoTax * (100 + currentItem.tax) / 100;
+    this.currentReceipt.totalPriceTax += currentItem.priceTax;
+
+    // Add the new item to the receipt
+    if (!this.currentReceipt.items) {
+      this.currentReceipt.items = [];
+    }
+    if (this.getItemIndex(currentItem.name) === -1) {
+      currentItem.quantity = 1;
+      this.currentReceipt.items.push(currentItem);
+    }
+    else {
+      this.currentReceipt.items[this.getItemIndex(currentItem.name)].quantity++;
+    }
+    console.log(this.currentReceipt);
   }
 
 }
