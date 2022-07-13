1e97fe2b0cc7489df41343939dc0e0d2f28c65ff
Fedja Vujicic <fedjavujicic@gmail.com>
2022-07-13 20:10:58 +0200

    Table selection logic with receipt storing

--- frontend/src/app/components/company-pages/table-selection/table-selection.component.ts
+++ frontend/src/app/components/company-pages/table-selection/table-selection.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Company } from 'src/app/models/company';
 import { Department } from 'src/app/models/department';
+import { Receipt } from 'src/app/models/receipt';
 import { Table } from 'src/app/models/table';
 
 @Component({
@@ -19,30 +21,49 @@ export class TableSelectionComponent implements OnInit {
   company: Company;
   allTables: Array<Table> = [];
   allDepartments: Array<Department> = [];
+  allReceipts: Array<Receipt>;
   currentDepartment: number = 0;
+  message: string;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
     this.company = JSON.parse(localStorage.getItem("logged"));
     this.allDepartments = this.company.departments;
+    this.allReceipts = JSON.parse(localStorage.getItem("allReceipts"));
   }
 
   getStyle(i: number) {
-      return {
-        width: "10vw",
-        height: "10vw",
-        'background-color': "#815438",
-        border: "3px solid black",
-        position: "absolute",
-        top: this.allDepartments[this.currentDepartment].tables[i].top,
-        left: this.allDepartments[this.currentDepartment].tables[i].left,
-        'border-radius': this.allDepartments[this.currentDepartment].tables[i].borderRadius,
-      }
+    return {
+      width: "10vw",
+      height: "10vw",
+      'background-color': "#815438",
+      border: "3px solid black",
+      position: "absolute",
+      top: this.allDepartments[this.currentDepartment].tables[i].top,
+      left: this.allDepartments[this.currentDepartment].tables[i].left,
+      'border-radius': this.allDepartments[this.currentDepartment].tables[i].borderRadius,
+    }
   }
 
-  selectTable(table: Table, index: number) {
-    console.log("clicked table" + index);
+  departmentName(): string {
+    return this.allDepartments[this.currentDepartment].name;
+  }
+
+  selectTable(index: number) {
+    if (this.allReceipts) {
+      if (this.allReceipts.find(receipt => {
+        (receipt.tableId === index) && (receipt.departmentName === this.departmentName())
+      })) {
+        this.message = "Zauzet!";
+        return;
+      }
+    }
+    let currentReceipt: Receipt = new Receipt();
+    currentReceipt.tableId = index;
+    currentReceipt.departmentName = this.departmentName();
+    localStorage.setItem("currentReceipt", JSON.stringify(currentReceipt));
+    this.router.navigate(["company/receipts-caterer"]);
   }
 
 
